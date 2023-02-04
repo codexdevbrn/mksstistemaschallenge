9b0a2aa16e5456e7f3cc627bc43b4d244fba904a
Bruno Duarte <100212947+codexdevbrn@users.noreply.github.com>
2023-02-03 21:32:39 -0300

    Update fecthProducts.ts

--- src/utils/fecthProducts.ts
+++ src/utils/fecthProducts.ts
@@ -8,7 +8,7 @@ export interface ProductsResponse {
 /* Busca os produtos na API */
 export function fetchProducts(page: number, options?: RequestInit) {
   return fetchWrapper<ProductsResponse>(
-    `https://mks-challenge-api-frontend.herokuapp.com/api/v1/products?page=8&rows=8&sortBy=name&orderBy=ASC`,
+    `https://mks-challenge-api-frontend.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC`,
     options,
   );
 }
